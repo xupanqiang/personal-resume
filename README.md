40ad1da92c73d729dea2cfc59d4b87f5629b7780
xu <541592004@qq.com>
2018-09-29 16:13:47 +0800

    webpack更新

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# resume
-个人简历
+
+
+
+## 总体要求
+
+## 前端目录结构与说明
+
+### 命名要求
+1. 所有类(组件)文件遵循大驼峰书写格式。
+2. 其余文件遵循小驼峰书写格式。
+3. 包名遵循小驼峰书写格式。
+4. [其余规范查看。](http://www.jianshu.com/p/d359933374e3)
+
+
+```
+** 代表可选
+.
+├── dist                        # 文件打包后的存放目录
+├── config                      # 项目配置文件目录
+└── src                         # 项目源文件目录
+    ├── components              # 公共组件目录
+    ├── js                      # 全局js公共方法目录
+    ├── index.js                # 入口文件   
+    ├── routes                  # 主路由和异步路由分割点目录
+    │   ├── home                # 业务目录“根据业务自定义名称”目录
+    │   │   ├── assets **       # 组件引入的静态资源目录
+    │   │   │   └── xxx.png     # *** 图片 json 等文件
+    │   │   ├── components      # 业务组件目录
+    │   │   │   ├── Home.js     # 业务（组件）文件
+    │   │   │   └── home.less   # 业务样式文件
+    │   │   ├─ modules **      # actions/constants/reducers/saga的集合目录
+    │   │   └── routes **       # 子路由目录
+    │   └── index.js            # 路由集合文件
+    ├── store                   # Redux指定块目录
+    │   ├── createStore.js      # 创建和使用 redux store 文件
+    │   ├── reducers.js         # Reducer 注册和注入文件
+    │   └── sagas.js            # 合并 各个saga 的文件
+    └── styles                  # 公共样式目录
+
+### 涉及到的技术
+> react react-router redux immutable webpack axios less antd
+
+#### react
+react 是一个前端框架，提供 UI 界面。
+
+#### react-router
+与 react 结合提供网站页面导航。
+
+#### redux
+管理 react 的状态，react 是由状态驱动页面渲染。
+
+#### immutable
+将原生的 js 可变的数据结构，变为不可变的数据结构。在 react 周期函数 `shouldComponentUpdate(nextProps, nextState)`   进行属性和状态的判断，从而进行有选择性的渲染页面。 `shouldComponentUpdate` 方法默认总是返回 `true`。
+
+#### webpack
+用于打包项目，拆分业务代码，按需加载等。
+
+#### axios
+ajax请求。
+
+#### less
+负责处理样式。
+
+#### antd
+基于 react 封装的一套 UI 组件。
+
+
+
+
+# 注意
+  如果在build或者start的时候出现node-sass之类的报错，可以使用 npm rebuild node-sass来处理下，再build或start，有不了解的同学可以issues告诉我。
+
+# 使用方法：
+    安装模块：npm install
+
+    开发模式：npm start
+
+    打包：npm run build
+
+    访问地址：http://localhost:3012
+
+# 打包上线
+* 将webpack.config.js中的publicPath的 ./ 改成你线上的绝对地址，比如 http://129.0.0.111 之类，然后再执行npm run build
+* 打包后直接将build文件夹提交至你们项目得根目录中
+* 命令并且呵斥你们得后端人员，将404指向build/index.html，这样基本就可以愉快得查看了
+* 如果在上线后首屏调用速度较慢，这样得情况得话，可以鞭挞后端人员，开启服务器得gzip压缩功能将js压缩一下，压缩后大小基本只有原来得三分之一，这个很实用
--- webpack.config.js
+++ webpack.config.js
@@ -1,300 +1,134 @@
 const path = require('path');
-const webpack = require('webpack');
-const config = require('config');
-// 拆 css
-const ExtractTextPlugin = require('mini-css-extract-plugin');
-// gzip 压缩
-const CompressionPlugin = require('compression-webpack-plugin');
-// bundle 大小分析
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-// 开发模式
-// const DEV = process.env.NODE_ENV === 'development';
-// 生产模式
-const PROD = process.env.NODE_ENV === 'production';
-// 只有在开发模式下启用 source-map
-const devtool = PROD ? false : "source-map";
-// 指定 filename
-const filename = PROD ? '[name].js' : 'bundle.js';
-// 提取项目内的公共 js
-const entry = PROD ? {
-    bundle: './src/index.js',
-    babelPolyfill: 'babel-polyfill',
-    constants: ['./src/constants/statusCode.js', './src/constants/sysConstants.js', './src/constants/url.js'],
-    cjs: ['./src/js/globalJs.js', './src/js/msgReducer.js', './src/js/requestUtil.js', './src/js/sagaUtil.js', './src/js/sysMsgUtil.js', './src/js/uploadUtil.js'],
-    area: './src/routes/po/components/Area.js',
-    aTable: 'antd/lib/table',
-    aDatePicker: 'antd/lib/date-picker'
-} : [
-    './src/index.js',
-];
-// 提取项目中 css
-const extractCSS = new ExtractTextPlugin({
-    filename: 'stylesheets/[name]-one.css',
-    allChunks: true
-});
-// 提取项目中 less
-const extractLESS = new ExtractTextPlugin({
-    filename: 'stylesheets/[name]-two.css',
-    allChunks: true
-});
-const plugins = PROD ? [
-    // 导出 css
-    extractCSS,
-    // 导出 less
-    extractLESS,
-    // 提取公共 js
-    // new webpack.optimize.CommonsChunkPlugin({
-    //     names: ['aDatePicker', 'aTable', 'area', 'cjs', 'constants', 'babelPolyfill'],
-    //     filename: "js/CommonsLibs/[name].chunk.js",
-    //     minChunks: Infinity
-    // }),
+const webpack = require("webpack");
+var pro = process.env.NODE_ENV == "production" ? true : false
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+//开发环境端口号
+var dev_port = "8010"
+//更改本地测试环境的地址，可以写localhost，或者写你本地的ip方便手机测试
+var url = "localhost"
 
 
-    // 丑化js
-    // new webpack.optimize.UglifyJsPlugin({
-    //     minimize: true,
-    //     mangle: false,
-    //     output: {
-    //         // 最紧凑的输出
-    //         beautify: false,
-    //         // 去掉注释
-    //         comments: false,
-    //         screw_ie8: false
-    //     },
-    //     compress: {
-    //         warnings: false,
-    //         // 删除conslos.*
-    //         drop_console: true,
-    //     },
-    //     warnings: false
-    // }),
+//不同环境加载不同的插件
+let plg = [];
+if (pro) {
+    plg = [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '/src/index.html') // Load a custom template
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[hash].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 
-    // new webpack.optimize.SplitChunksPlugin({
-    //     cacheGroups: {
-    //         names: ['aDatePicker', 'aTable', 'area', 'cjs', 'constants', 'babelPolyfill'],
-    //         filename: "js/CommonsLibs/[name].chunk.js",
-    //         minChunks: Infinity
-    //         default: {
-    //             minChunks: 2,
-    //             priority: -20,
-    //             reuseExistingChunk: true,
-    //         },
-    //         打包重复出现的代码
-    //         vendor: {
-    //             chunks: 'initial',
-    //             minChunks: 2,
-    //             maxInitialRequests: 5, // The default limit is too small to showcase the effect
-    //             minSize: 0, // This is example is too small to create commons chunks
-    //             name: 'vendor'
-    //         },
-    //         打包第三方类库
-    //         commons: {
-    //             names: ['aDatePicker', 'aTable', 'area', 'cjs', 'constants', 'babelPolyfill'],
-    //             filename: "js/CommonsLibs/[name].chunk.js",
-    //             chunks: "initial",
-    //             minChunks: Infinity
-    //         }
-    //     }
-    // }),
 
-    new webpack.optimize.RuntimeChunkPlugin({
-        name: "manifest"
-    }),
-    // 设置此模式为生产模式
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    // gzip 压缩
-    new CompressionPlugin({
-        asset: "[path].gz[query]",
-        algorithm: "gzip",
-        test: /\.js$|\.css$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.8
-    })
-    // new BundleAnalyzerPlugin()
-] : [
-    // 该插件帮助我们安心地使用环境变量
-    // 设置此模式为开发模式
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('development')
-    })
-];
-const cssIdentName = PROD ? '[hash:base64:10]' : '[path][name]-[local]-[hash:base64:24]';
-const cssLoader = PROD ?
-    extractCSS.extract({
-        fallback: "style-loader",
-        use: 'css-loader?localIdentName=' + cssIdentName
-    })
-    : [
-        'style-loader', 'css-loader?localIdentName=' + cssIdentName
-    ];
-const lessLoader = PROD ?
-    extractLESS.extract({
-        fallback: "style-loader",
-        use: [{
-            loader: 'css-loader'
-        }, {
-            // modifyVars 更改全局样式
-            loader: 'less-loader?{modifyVars:{"@primary-color":"#0d85db", "@font-size-base":"12px"}}',
-            options: {javascriptEnabled: true}
-        }]
-    })
-    : [
-        {
-            loader: 'style-loader'
-        },
-        {
-            loader: 'css-loader'
-        },
-        {
-            loader: 'less-loader?{modifyVars:{"@primary-color":"#0d85db", "@font-size-base":"12px"}}',
-            options: {javascriptEnabled: true}
-        }
-    ];
+} else {
+    plg = [
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
+
 
 module.exports = {
-    devtool: devtool,
-    entry: entry,
-    output: {
-        // 打包文件名
-        filename: filename,
-        // bundle.js 输出路径,是一个绝对路径
-        path: path.join(__dirname, 'dist'),
-        // 在 html 页面中需要的导入资源的路径
-        publicPath: '/dist/',
-        // 设置按需加载 js 的名文件名
-        chunkFilename: "[name].[hash:8].chunk.js"
+    entry: {
+        index: ['babel-polyfill', './src/index.js']
     },
-    mode:'development',
-    plugins: plugins,
-    module: {
-        rules: [
-            // 解析装载 js
-            {
-                test: /\.(js|jsx)$/,
-                exclude: '/node_modules/',
-                loader: 'babel-loader',
-                query: {
-                    // 加入 antd 模块，并使支持 less
-                    // plugins: [
-                    //     ['import', [{libraryName: 'antd', style: true}]], // antd 按需引入
-                    //     ["transform-decorators-legacy"], // redux @connect
-                    // ],
-                    cacheDirectory: true,
-                },
-            },
-            // 解析装载图片，等文件
-            {
-                test: /\.(jpg|gif|png)$/,
-                exclude: '/node_modules/',
-                // loader: 'file-loader'
-                loader: 'url-loader?limit=512&name=[path][name].[ext]?[hash]',
-            },
-            // 解析装载 css
-            {
-                test: /\.css$/,
-                exclude: '/node_modules/',
-                use: cssLoader,
-            },
-            // 解析装载 less
-            {
-                test: /\.less$/,
-                exclude: '/node_modules/',
-                use: lessLoader,
-
-            },
-            // 解析 iconfont
-            {
-                test: /\.(woff|eot|svg|ttf|ppt|pdf|doc)\??.*$/,
-                loader: "url-loader?name=fonts/[name].[md5:hash:hex:7].[ext]"
+    optimization: {
+        splitChunks: {
+            chunks: "initial",         // 必须三选一： "initial" | "all"(默认就是all) | "async"
+            minSize: 0,                // 最小尺寸，默认0
+            minChunks: 1,              // 最小 chunk ，默认1
+            maxAsyncRequests: 1,       // 最大异步请求数， 默认1
+            maxInitialRequests: 1,    // 最大初始化请求书，默认1
+            name: () => {
+            },              // 名称，此选项课接收 function
+            cacheGroups: {                 // 这里开始设置缓存的 chunks
+                priority: "0",                // 缓存组优先级 false | object |
+                vendor: {                   // key 为entry中定义的 入口名称
+                    chunks: "initial",        // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+                    test: /react|lodash/,     // 正则规则验证，如果符合就提取 chunk
+                    name: "vendor",           // 要缓存的 分隔出来的 chunk 名称
+                    minSize: 0,
+                    minChunks: 1,
+                    enforce: true,
+                    maxAsyncRequests: 1,       // 最大异步请求数， 默认1
+                    maxInitialRequests: 1,    // 最大初始化请求书，默认1
+                    reuseExistingChunk: true   // 可设置是否重用该chunk（查看源码没有发现默认值）
+                }
             }
-        ]
+        }
     },
-
-    // optimization: {
-    //     splitChunks: {
-    //         chunks: "initial",         // 必须三选一： "initial" | "all"(默认就是all) | "async"
-    //         minSize: 0,                // 最小尺寸，默认0
-    //         minChunks: 1,              // 最小 chunk ，默认1
-    //         maxAsyncRequests: 1,       // 最大异步请求数， 默认1
-    //         maxInitialRequests: 1,    // 最大初始化请求书，默认1
-    //         name: () => {},              // 名称，此选项课接收 function
-    //         cacheGroups: {                 // 这里开始设置缓存的 chunks
-    //             priority: "0",                // 缓存组优先级 false | object |
-    //             vendor: {                   // key 为entry中定义的 入口名称
-    //                 chunks: "initial",        // 必须三选一： "initial" | "all" | "async"(默认就是异步)
-    //                 test: /react|lodash/,     // 正则规则验证，如果符合就提取 chunk
-    //                 name: "vendor",           // 要缓存的 分隔出来的 chunk 名称
-    //                 minSize: 0,
-    //                 minChunks: 1,
-    //                 enforce: true,
-    //                 maxAsyncRequests: 1,       // 最大异步请求数， 默认1
-    //                 maxInitialRequests: 1,    // 最大初始化请求书，默认1
-    //                 reuseExistingChunk: true   // 可设置是否重用该chunk（查看源码没有发现默认值）
-    //             }
-    //         }
-    //     }
-    // },
-    // optimization: {
-    //     splitChunks: {
-    //         text: /react|lodash/,
-    //         names: ['aDatePicker', 'aTable', 'area', 'cjs', 'constants', 'babelPolyfill'],
-    //         filename: "js/CommonsLibs/[name].chunk.js",
-    //         enforce: true,
-    //         chunks: "all",
-    //         // minChunks: Infinity,
-    //         // minimize: true,
-    //         // mangle: false,
-    //         // output: {
-    //         //     // 最紧凑的输出
-    //         //     beautify: false,
-    //         //     // 去掉注释
-    //         //     comments: false,
-    //         //     screw_ie8: false
-    //         // },
-    //         // compress: {
-    //         //     warnings: false,
-    //         //     // 删除conslos.*
-    //         //     drop_console: true,
-    //         // },
-    //         // warnings: false
-    //     }
-    // },
-    // 剔除的js生产环境时需要从 cdn 引入
-    externals: PROD ? {
-        'react': 'React',
-        'react-dom': 'ReactDOM',
-        "react-router": "ReactRouter",
-        'redux': 'Redux',
-        'react-redux': 'ReactRedux',
-        'immutable': 'Immutable',
-        'moment': 'moment',
-        'moment/locale/zh-cn': 'moment.locale',
-    } : {},
-    devServer: {
-        host: '0.0.0.0',
-        port: 8088,
-        // hot: true,
-        publicPath: '/dist/',
-        stats: {
-            colors: true
-        },
-        inline: true,
-        // 代理
-        /*proxy: {
-         '/api': {
-         target: 'http://192.168.1.249:8000',
-         changeOrigin: true,
-         secure: false,
-         }
-         },*/
-        historyApiFallback: true,
-        disableHostCheck: true,
-        // contentBase: './',
+    plugins:plg,
+    resolve: {
+        extensions: ['.js', '.jsx', '.less', '.scss', '.css'],
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+            path.join(__dirname, './src')
+        ],
+        alias: {
+            // "actions": path.resolve(__dirname, "src/actions"),
+            // "components": path.resolve(__dirname, "src/components"),
+            // "containers": path.resolve(__dirname, "src/containers"),
+            // "reducers": path.resolve(__dirname, "src/reducers"),
+            // "utils": path.resolve(__dirname, "src/utils")
+        }
     },
-    performance: {
-        hints: false,
+    output: {
+        filename: pro ? 'bundle.js' : '[name].js',//'[name].[hash].js' : '[name].js'
+        path: path.join(__dirname, 'dist'),
+        publicPath: pro ? './' : `http://${url}:${dev_port}/dist/`
     },
-    cache: false,
+    devtool: false,
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /(node_modules)/,
+            use: ['babel-loader']
+        }, {
+            test: /\.(less|css)$/,
+            use: [MiniCssExtractPlugin.loader, {
+                loader: 'css-loader?importLoaders=1',
+                options: {
+                    minimize: true //css压缩
+                }
+            }, {loader: 'less-loader', options: {javascriptEnabled: true}}]
+        }, {
+            test: /\.(png|jpg|gif|md)$/,
+            use: ['file-loader?limit=10000&name=[md5:hash:base64:10].[ext]']
+        }, {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            use: ['url-loader?limit=10000&mimetype=images/svg+xml']
+        }, {
+            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "file-loader"
+        }, {
+            test: /\.json$/,
+            use: "json-loader"
+        }]
+    },
+    devServer: {//webpack-dev-server配置热更新以及跨域
+        historyApiFallback: true,//不跳转
+        noInfo: true,
+        inline: true,//实时刷新
+        host:url,
+        port: dev_port,
+        hot: true,
+        // proxy: {
+        //     '/list': {
+        //         target: 'http://lol.qq.com/web201310/js/videodata/LOL_VIDEOLIST_IDX3.js',
+        //         pathRewrite: {'^/list': ''},
+        //         changeOrigin: true,
+        //         secure: false
+        //     }
+        // }
+    }
 };
